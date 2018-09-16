35917e127657c3c73df1ef28e8cf094ae5223369
Olexandr Rizhiy <orizhiy@e1r8p9.unit.ua>
2018-09-16 15:42:41 +0300

    done

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -1,57 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orizhiy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/16 15:28:07 by orizhiy           #+#    #+#             */
+/*   Updated: 2018/09/16 15:28:09 by orizhiy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+void	ft_putchar(char c);
 
-void ft_putchar(char c);
-
-int	ft_strcmp(char *s1, char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
-    while (*s1 || *s2)
-    {
-        if (*s1 != *s2)
-            return (*s1 - *s2);
-        s1++;
-        s2++;
-    }
-    if ((!(*s1) && *s2) || (!(*s2) && *s1))
-        return (*s1 - *s2);
-    return (0);
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if ((!(*s1) && *s2) || (!(*s2) && *s1))
+		return (*s1 - *s2);
+	return (0);
 }
 
-void	ft_printer(int argc, char *argv[])
+void	ft_printer(int argc, char **argv)
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i++ < argc - 1)
-    {
-        j = 0;
-        while (argv[i][j])
-        {
-            ft_putchar(argv[i][j]);
-            j++;
-        }
-        ft_putchar('\n');
-    }
+	i = 0;
+	while (i++ < argc - 1)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+	}
 }
 
-void ft_str_swap(char ***argv, int argc, int *i)
+void	ft_str_swap(char **av, int *i)
 {
-    *(argv[argc + 1]) = (*argv[i]);
-    *(argv[i]) = *(argv[i + 1]);
-    *(argv[i + 1]) = *(argv[argc + 1]);
-    *i = 0;
+	av[0] = av[*i + 1];
+	av[*i + 1] = av[*i];
+	av[*i] = av[0];
+	*i = 0;
 }
 
-int		main(int argc, char *argv[])
+int		main(int ac, char **av)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (i++ < (argc - 1))
-    {
-        if (i + 1 < argc && ft_strcmp(argv[i + 1], argv[i]) < 0)
-            ft_str_swap(&argv, argc, &i)
-    }
-    ft_printer(argc, argv);
-    return (0);
+	i = 1;
+	while (i++ < (ac - 1))
+		if (i + 1 < ac && ft_strcmp(av[i + 1], av[i]) < 0)
+			ft_str_swap(av, &i);
+	ft_printer(ac, av);
+	return (0);
 }
--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,20 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orizhiy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/16 15:28:28 by orizhiy           #+#    #+#             */
+/*   Updated: 2018/09/16 15:28:30 by orizhiy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int		ft_strlen(char *str)
 {
-	char *p;
+	char	*p;
 
 	p = str;
 	while (*p)
 		p++;
-	return (p-str);
+	return (p - str);
 }
 
 char	*ft_strdup(char *src)
 {
 	char	*dup;
-	char    *res;
+	char	*res;
 
 	if (!(dup = (char *)malloc(sizeof(*dup) * ft_strlen(src) + 1)))
 		return (NULL);
